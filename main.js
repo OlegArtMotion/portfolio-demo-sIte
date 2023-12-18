3433eeb823c08fb77cd5d3e57ac7d0e60f6ec907
Oleg Ukrainski <oleg.art.motion@gmail.com>
2023-12-18 19:13:32 +0200

    added new folder

---
+++ main.js
@@ -0,0 +1,10 @@
+var kids = ['erik', 'lola', 'amichay', 'israela'];
+
+let found = false;
+for(let i = 0; i < kids.length; i++) {
+  if (kids[i] === 'lola') {
+    found = true;
+  }
+ 
+}
+ return found;
